a14e4842944c847ae65e288baf3ec832acb0b149
dlvera <dlvera@xetid.cu>
2025-03-26 15:12:01 +0100

    rectificando los requeriments

---
+++ core/users/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+class LoginForm(AuthenticationForm):
+    remember_me = forms.BooleanField(required=False)
